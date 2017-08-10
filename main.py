b326d07bc1ec5cd297645691848b2268727c49eb
k2ev <30880794+k2ev@users.noreply.github.com>
2017-08-10 11:27:57 -0400

    n-way merge

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
+import copy
 import random
 from time import time
-from bubble import *
-from insertion import *
+
 from merge import *
-from quick import *
-import copy
+
 
 def main():
-    #a = [9,1,5,9,2,5]
-    a = random.choices(range(0,100000000), k=10000000)
+    # a = [27, 24, 23, 26, 24]
+    a = random.choices(range(0, 10000000), k=1000000)
     a1 = copy.copy(a)
     print("original array")
     # print(a)
     t1 = time()
-    quick_sort(a, switch_threshold=8)
+    merge_sort(a, switch_threshold=8)
     t2 = time()
     print("sorted array time", t2-t1)
-    # print(a)
+    print(a)
     print("original array")
     # print(a1)
     t3 = time()
-    merge_sort(a1, switch_threshold=8)
+    merge_sort(a1, num_part=3, switch_threshold=8)
     t4 = time()
     print("sorted array time", t4 - t3)
-    # print(a1)
+    print(a1)
+    print(a == a1)
 
 if __name__ == "__main__":
     main()
--- merge.py
+++ merge.py
@@ -1,58 +1,113 @@
-from utils import my_length
 from insertion import insertion_sort
+from utils import my_length
 
 
 # wrapper
-def merge_sort(my_list, left=0, right=None, switch_threshold=0):
-    # my_merge_sort_r(my_list, left, right, switch_threshold)
-    my_merge_sort_nr(my_list, left, right, switch_threshold)
+def merge_sort(my_list, left=0, right=None, num_part=2, switch_threshold=0):
+    my_merge_sort_r(my_list, left, right, num_part, switch_threshold)
+    # my_merge_sort_nr(my_list, left, right, switch_threshold)
 
 
 # merge sort another style
-def my_merge_sort_r(my_list, left=0, right=None, switch_threshold=0):
+def my_merge_sort_r(my_list, left=0, right=None, num_part=2, switch_threshold=0):
     left, right, length = my_length(my_list, left, right)
 
-    if length < switch_threshold:
+    if length < switch_threshold or num_part < 2:
         insertion_sort(my_list, left, right)
+        return
 
     if length > 1:
-        mid = left + (right - left) // 2
-        my_merge_sort_r(my_list, left, mid)
-        my_merge_sort_r(my_list, mid + 1, right)
-        my_merge(my_list, left, mid, right)
+        if num_part == 2:
+            mid = left + (right - left) // 2
+            my_merge_sort_r(my_list, left, mid, num_part, switch_threshold)
+            my_merge_sort_r(my_list, mid + 1, right, num_part, switch_threshold)
+            my_merge(my_list, left, right, mid)
+        else:
+            part_size = max(length // num_part, 1)  # min part_size permitted is 1
+            lo = left
+            mids = []
+            while lo <= right:
+                mid = min(lo - 1 + part_size, right)
+                if mid > right - part_size:
+                    mid = right  # dont append to mids as this is last one
+                else:
+                    mids.append(mid)
+                my_merge_sort_r(my_list, lo, mid, num_part, switch_threshold)
+                lo = mid + 1
+
+            my_merge_n_ways(my_list, left, right, mids)
 
     return
 
 
-def my_merge(my_list, left, mid, right):
+# two ways merge - this is somewhat suboptimal as there is a lot of copying happening here
+def my_merge(my_list, left, right, mid):
     length = right - left + 1
     temp = [0] * length
-    i, j, k = left, mid + 1, 0
+    i, j, pos = left, mid + 1, 0
 
-    while i <= mid and j <= right:
-        if my_list[i] <= my_list[j]:
-            temp[k] = my_list[i]
+    while pos < length:
+        if i <= mid and j <= right:
+            if my_list[i] <= my_list[j]:
+                temp[pos] = my_list[i]
+                i += 1
+            else:
+                temp[pos] = my_list[j]
+                j += 1
+        elif i <= mid:
+            temp[pos] = my_list[i]
             i += 1
-        else:
-            temp[k] = my_list[j]
+        elif j <= right:
+            temp[pos] = my_list[j]
             j += 1
-        k += 1
 
-    while i <= mid:
-        temp[k] = my_list[i]
-        i += 1
-        k += 1
-
-    while j <= right:
-        temp[k] = my_list[j]
-        j += 1
-        k += 1
+        pos += 1
 
     for idx in range(length):
         my_list[left + idx] = temp[idx]
 
     return
 
+
+# returns lowest value in the partitions and index of partition
+def partitions_lowest(my_list, start, end):
+    min_idx_p, min_val = None, None  # initialize
+    for index in range(len(start)):
+        if start[index] <= end[index]:
+            index_pos = start[index]
+            if min_val is None or my_list[index_pos] <= min_val:
+                min_val = my_list[index_pos]
+                min_idx_p = index  # partition index
+    return min_idx_p
+
+
+# n ways merge - this is somewhat suboptimal as there is a lot of copying happening here
+def my_merge_n_ways(my_list, left, right, partitions):
+    length = right - left + 1
+
+    if length > 1 and len(partitions) > 0:
+        temp = [0] * length
+        partitions_start = [left]
+        partitions_end = []
+        for p in partitions:
+            partitions_end.append(p)
+            partitions_start.append(p + 1)
+        partitions_end.append(right)
+
+        pos = 0
+        while pos < length:
+            p_i = partitions_lowest(my_list, partitions_start, partitions_end)
+            if p_i is None:
+                break
+            else:
+                temp[pos] = my_list[partitions_start[p_i]]
+                partitions_start[p_i] += 1
+                pos += 1
+
+        for idx in range(length):
+            my_list[left + idx] = temp[idx]
+
+    return
 
 # merge sort non recursive
 def my_merge_sort_nr(my_list, left=0, right=None, switch_threshold=0):
@@ -73,8 +128,8 @@ def my_merge_sort_nr(my_list, left=0, right=None, switch_threshold=0):
             left_p = left
             while left_p <= right:
                 mid_p = min(left_p + width - 1, right)
-                right_p = min(left_p + 2*width - 1, right)
-                my_merge(my_list, left_p, mid_p, right_p)
+                right_p = min(left_p + 2 * width - 1, right)
+                my_merge(my_list, left_p, right_p, mid_p)
                 left_p += 2*width
             width *= 2
 
