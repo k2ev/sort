92425d696c45b48b033dfd91f305ffdf47a3eea6
k2ev <30880794+k2ev@users.noreply.github.com>
2017-08-18 11:03:02 -0400

    n-way merge fixed but inefficient

--- merge.py
+++ merge.py
@@ -3,13 +3,13 @@ from utils import my_length
 
 
 # wrapper
-def merge_sort(my_list, left=0, right=None, num_part=2, switch_threshold=0):
-    my_merge_sort_r(my_list, left, right, num_part, switch_threshold)
+def merge_sort(my_list, left=0, right=None, num_part=2, switch_threshold=0, n_ways=False):
+    my_merge_sort_r(my_list, left, right, num_part, switch_threshold, n_ways)
     # my_merge_sort_nr(my_list, left, right, switch_threshold)
 
 
 # merge sort another style
-def my_merge_sort_r(my_list, left=0, right=None, num_part=2, switch_threshold=0):
+def my_merge_sort_r(my_list, left=0, right=None, num_part=2, switch_threshold=0, n_ways=False):
     left, right, length = my_length(my_list, left, right)
 
     if length < switch_threshold or num_part < 2:
@@ -17,10 +17,10 @@ def my_merge_sort_r(my_list, left=0, right=None, num_part=2, switch_threshold=0)
         return
 
     if length > 1:
-        if num_part == 2:
+        if num_part == 2 and n_ways is False:
             mid = left + (right - left) // 2
-            my_merge_sort_r(my_list, left, mid, num_part, switch_threshold)
-            my_merge_sort_r(my_list, mid + 1, right, num_part, switch_threshold)
+            my_merge_sort_r(my_list, left, mid, num_part, switch_threshold, n_ways)
+            my_merge_sort_r(my_list, mid + 1, right, num_part, switch_threshold, n_ways)
             my_merge(my_list, left, right, mid)
         else:
             part_size = max(length // num_part, 1)  # min part_size permitted is 1
@@ -32,7 +32,7 @@ def my_merge_sort_r(my_list, left=0, right=None, num_part=2, switch_threshold=0)
                     mid = right  # dont append to mids as this is last one
                 else:
                     mids.append(mid)
-                my_merge_sort_r(my_list, lo, mid, num_part, switch_threshold)
+                my_merge_sort_r(my_list, lo, mid, num_part, switch_threshold, n_ways)
                 lo = mid + 1
 
             my_merge_n_ways(my_list, left, right, mids)
@@ -70,44 +70,65 @@ def my_merge(my_list, left, right, mid):
 
 
 # returns lowest value in the partitions and index of partition
-def partitions_lowest(my_list, start, end):
+def partitions_lowest(my_list, start, end, length_partitions) -> int:
     min_idx_p, min_val = None, None  # initialize
-    for index in range(len(start)):
-        if start[index] <= end[index]:
-            index_pos = start[index]
-            if min_val is None or my_list[index_pos] <= min_val:
-                min_val = my_list[index_pos]
+    for index in range(length_partitions + 1):
+        if start[index] != -1:
+            if min_val is None or my_list[start[index]] <= min_val:
+                min_val = my_list[start[index]]
+                max_val = my_list[end[index]]
                 min_idx_p = index  # partition index
-    return min_idx_p
+
+    if min_idx_p is None:
+        raise ValueError("No partition found")
+
+    flag_partition_all_lower = True
+    for index in range(length_partitions + 1):
+        if index != min_idx_p and start[index] != -1 and my_list[start[index]] < max_val:
+            flag_partition_all_lower = False
+            break
+    return min_idx_p, flag_partition_all_lower
+
+
 
 
 # n ways merge - this is somewhat suboptimal as there is a lot of copying happening here
 def my_merge_n_ways(my_list, left, right, partitions):
     length = right - left + 1
+    length_partitions = len(partitions)
 
-    if length > 1 and len(partitions) > 0:
-        temp = [0] * length
-        partitions_start = [left]
-        partitions_end = []
-        for p in partitions:
-            partitions_end.append(p)
-            partitions_start.append(p + 1)
+    if length > 1 and length_partitions > 0:
+
+        partitions_start = list(map(lambda x: x + 1, partitions))
+        partitions_start.insert(0, left)
+        partitions_end = partitions
         partitions_end.append(right)
 
         pos = 0
+        temp = [0] * length
         while pos < length:
-            p_i = partitions_lowest(my_list, partitions_start, partitions_end)
+            p_i, flag_lowest = partitions_lowest(my_list, partitions_start, partitions_end, length_partitions)
             if p_i is None:
                 break
             else:
-                temp[pos] = my_list[partitions_start[p_i]]
-                partitions_start[p_i] += 1
-                pos += 1
+                if flag_lowest:
+                    for i in range(partitions_start[p_i], partitions_end[p_i] + 1):
+                        temp[pos] = my_list[i]
+                        pos += 1
+                    partitions_start[p_i] = -1
+                else:
+                    temp[pos] = my_list[partitions_start[p_i]]
+                    if partitions_start[p_i] < partitions_end[p_i]:
+                        partitions_start[p_i] += 1
+                    else:
+                        partitions_start[p_i] = -1  # switch off this partition
+                    pos += 1
 
         for idx in range(length):
             my_list[left + idx] = temp[idx]
 
     return
+
 
 # merge sort non recursive
 def my_merge_sort_nr(my_list, left=0, right=None, switch_threshold=0):
